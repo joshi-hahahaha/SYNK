a1d94a4fdea01424e4257c758398246d585f78d1
justin <justinlew66@gmail.com>
2025-03-25 00:50:40 +1100

    Removed email validator

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -5,7 +5,6 @@ import os
 from dotenv import load_dotenv
 import hashlib
 import re
-from email_validator import validate_email, EmailNotValidError
 import jwt
 import datetime
 
@@ -36,7 +35,7 @@ def register(users):
     password = data.get("password")
     email = data.get("email")
     reconfirmPassword = data.get("reconfirmPassord")
-    
+
     password = hashlib.sha256(password.encode()).hexdigest()
 
 
@@ -45,7 +44,7 @@ def register(users):
     # Required fields are missing
     if not firstName or not lastName or not username or not password or not reconfirmPassword:
         return jsonify({"message": "All fields are required"}), 400
-    
+
 
     # firstName limitations
     if len(firstName) < 2 or len(firstName) > 20:
@@ -54,7 +53,7 @@ def register(users):
     match = invalid_character.match(firstName)
     if not match:
         return jsonify({"message": "First name uses inappropriate characters"}), 400
- 
+
 
     # lastName limitations
     if len(lastName) < 2 or len(lastName) > 20:
@@ -73,21 +72,15 @@ def register(users):
     if existing_username:
         return jsonify({"message": "Username has been taken! Please enter a new username"}), 409
 
-    # email limitations
-    valid_email = validate_email(email)
-    if not valid_email:
-        return jsonify({"message": "Invalid email inputted"}), 400
-    
     existing_email = users.find_one({"email": email})
     if existing_email:
         return jsonify({"message": "Email already exists! Please provide another email"}), 409
- 
+
 
     # password limitations
     if password != reconfirmPassword:
         return jsonify({"message": "Passwords does not match up! Please retype your password"}), 400
 
-    
     user_id = users.insert_one({
         "firstName": firstName,
         "lastName": lastName,
