685b39f8c838c3107de159f9ac0114f8f1d92934
joshi-hahahaha <joshi.ha7@gmail.com>
2025-03-25 14:36:17 +1100

    IMP: basic button components.

---
+++ frontend/src/components/button/ButtonMenu.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ButtonMenu() {
+  return <div>ButtonMenu</div>;
+}
+
+export default ButtonMenu;
---
+++ frontend/src/components/button/Settingsbutton.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Settingsbutton() {
+  return <div>Settingsbutton</div>;
+}
+
+export default Settingsbutton;
