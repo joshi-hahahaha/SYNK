7edf62d8a4e6889b40f1acd9345763ed9990efff
justin <z5424659@ad.unsw.edu.au>
2025-03-25 17:44:43 +1100

    event markers

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -4,22 +4,6 @@ import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import {Icon} from "leaflet";
 
-const markerUserIcon = new Icon({
-  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  tooltipAnchor: [16, -28],
-})
-
-// const eventIcon = new Icon({
-//   iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
-//   iconSize: [25, 41],
-//   iconAnchor: [12, 41],
-//   popupAnchor: [1, -34],
-//   tooltipAnchor: [16, -28],
-// })
-
 const RecenterMap = ({ center }: { center: [number, number] }) => {
   const map = useMap();
   useEffect(() => {
@@ -28,8 +12,26 @@ const RecenterMap = ({ center }: { center: [number, number] }) => {
   return null;
 };
 
+const markerUserIcon = new Icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+})
+
+const eventIcon = new Icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+})
+
+
 const Map = () => {
   const [userLocation, setUserLocation] = useState<[number, number] | null>(null);
+  const [events, setEvents] = useState<any[]>([]);
 
   const successCallback = (position: GeolocationPosition) => {
     setUserLocation([position.coords.latitude, position.coords.longitude]);
@@ -41,9 +43,23 @@ const Map = () => {
       timeout: 1000 * 60,
       maximumAge: 0,
     });
-
     return () => navigator.geolocation.clearWatch(watchId);
   }, []);
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch("URL")
+      if (!response.ok) {
+        console.log("error")
+        return
+      }
+      const json = await response.json()
+      for (const event of json) {
+        setEvents([...events, event])
+      }
+    }
+    getData()
+  }, [])
 
   return (
     <div className="h-screen w-screen">
@@ -57,6 +73,11 @@ const Map = () => {
           <Marker position={userLocation} icon={markerUserIcon}>
             <Popup>{"You are here!"}</Popup>
           </Marker>
+          {events.map((event) => (
+            <Marker position={[event.lat, event.lon]} icon={eventIcon}>
+              <Popup>{event.name + "\n" + event.description}</Popup>
+            </Marker>
+          ))}
         </MapContainer>
       ) : (
         <p className="text-center mt-10">Fetching location...</p>
