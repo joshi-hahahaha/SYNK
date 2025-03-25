6fe922d25f4a3482ba2877885b7d2850e7ca4bf4
joshi-hahahaha <joshi.ha7@gmail.com>
2025-03-25 17:08:56 +1100

    UPD: modal with base information.

--- frontend/src/components/modal/EventsModal.tsx
+++ frontend/src/components/modal/EventsModal.tsx
@@ -4,8 +4,10 @@ const EventsModal = () => {
   return (
     <dialog id="events_modal" className="modal">
       <div className="modal-box">
-        <h3 className="font-bold text-lg">Hello!</h3>
-        <p className="py-4">Press ESC key or click outside to close</p>
+        <h3 className="font-bold text-2xl">Events</h3>
+        <p className="py-4">
+          This will have two tabs, one for the user events and the ones nearby
+        </p>
       </div>
       <form method="dialog" className="modal-backdrop">
         <button>close</button>
--- frontend/src/components/modal/SearchModal.tsx
+++ frontend/src/components/modal/SearchModal.tsx
@@ -4,8 +4,8 @@ const SearchModal = () => {
   return (
     <dialog id="search_modal" className="modal">
       <div className="modal-box">
-        <h3 className="font-bold text-lg">Hello!</h3>
-        <p className="py-4">Press ESC key or click outside to close</p>
+        <h3 className="font-bold text-lg">Search bar</h3>
+        <p className="py-4">This will be a search bar with a drop down</p>
       </div>
       <form method="dialog" className="modal-backdrop">
         <button>close</button>
