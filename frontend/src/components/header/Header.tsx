2b57c3df05443d6eb65a15967c8cf8f80e01704f
joshi-hahahaha <joshi.ha7@gmail.com>
2025-03-25 11:15:58 +1100

    IMP: header + ADD: daisyui.

---
+++ frontend/src/components/header/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="glass fixed top-0 left-0 w-full bg-base-100 text-base-content h-16 flex items-center justify-center text-2xl font-bold shadow-md z-[1000]">
+      SYNK
+    </div>
+  );
+}
+
+export default Header;
